d57966b38b60f91fd6ea677fc96b412b1be7895b
chanadech <61090003@kmitl.ac.th>
2021-03-13 03:51:11 +0700

    Initial commit

---
+++ app/src/main/java/activity/PresetAudioActivity.java
@@ -0,0 +1,16 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dechproduct.rebooapplication.R;
+
+public class PresetAudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preset_audio);
+    }
+}
